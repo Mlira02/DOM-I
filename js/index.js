a0ffa6fa65a5f9151e1dfe844d0449a78cb84b27
Allen Babiarz <ababiarz@Allen-Babiarzs-iMac.local>
2019-05-21 18:32:43 -0500

    Assignment near completion minus nav color change

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const siteContent = {
     "contact-h4" : "Contact",
     "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "email": "sales@greatidea.io",
   },
   "footer": {
     "copyright" : "Copyright Great Idea! 2018"
@@ -39,4 +39,48 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navi = document.querySelectorAll("a");
+navi[0].textContent = "Services";
+navi[1].textContent = "Product";
+navi[2].textContent = "Vision";
+navi[3].textContent = "Features";
+navi[4].textContent = "About";
+navi[5].textContent = "Contact";
+
+let headline = document.querySelector("h1");
+headline.textContent = "DOM Is Awesome"
+
+let button = document.querySelector("button");
+button.textContent = "Get Started";
+
+let logo2 = document.querySelector("#cta-img");
+logo2.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let h4 = document.querySelectorAll('h4');
+h4[0].textContent = "Features"
+h4[1].textContent = "About"
+h4[2].textContent = "Services"
+h4[3].textContent = "Product"
+h4[4].textContent = "Vision"
+h4[5].textContent = "Contact"
+
+let pTags = document.querySelectorAll("p");
+pTags[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pTags[5].textContent = "123 Way 456 Street Somewhere, USA";
+pTags[6].textContent = "1 (888) 888-8888";
+pTags[7].textContent = "sales@greatidea.io";
+pTags[8].textContent = "Copyright Great Idea! 2018";
+
+
+let logo_2 = document.getElementById("middle-img");
+logo_2.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+parentElement = document.querySelector('nav');
+let append1 = document.createElement('a');
+append1.textContent = "Item1";
+parentElement.appendChild(append1);
